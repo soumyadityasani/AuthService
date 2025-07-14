939494b27a65c553dad753677927234ed7a11aa2
Soumyaditya Mondal <souhttp@gmail.com>
2025-07-14 22:21:06 +0530

    All response are fixed ApiRespnseDto

--- src/main/java/com/canteen/authService/config/EnvInitializer.java
+++ src/main/java/com/canteen/authService/config/EnvInitializer.java
@@ -12,18 +12,38 @@ public class EnvInitializer implements ApplicationContextInitializer<Configurabl
                 .ignoreIfMissing()  // ✅ This is the key line
                 .load();
 
-        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+//        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+//        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+//        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+//        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+//        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+//
+//        System.setProperty("EMAIL_USER", dotenv.get("EMAIL_USER"));
+//        System.setProperty("EMAIL_PASS", dotenv.get("EMAIL_PASS"));
+//
+//        System.setProperty("TWILIO_SID", dotenv.get("TWILIO_SID"));
+//        System.setProperty("TWILIO_TOKEN", dotenv.get("TWILIO_TOKEN"));
+//        System.setProperty("TWILIO_NUM", dotenv.get("TWILIO_NUM"));
 
-        System.setProperty("EMAIL_USER", dotenv.get("EMAIL_USER"));
-        System.setProperty("EMAIL_PASS", dotenv.get("EMAIL_PASS"));
+        setIfPresent(dotenv, "DB_HOST");
+        setIfPresent(dotenv, "DB_PORT");
+        setIfPresent(dotenv, "DB_NAME");
+        setIfPresent(dotenv, "DB_USER");
+        setIfPresent(dotenv, "DB_PASS");
 
-        System.setProperty("TWILIO_SID", dotenv.get("TWILIO_SID"));
-        System.setProperty("TWILIO_TOKEN", dotenv.get("TWILIO_TOKEN"));
-        System.setProperty("TWILIO_NUM", dotenv.get("TWILIO_NUM"));
+        setIfPresent(dotenv, "EMAIL_USER");
+        setIfPresent(dotenv, "EMAIL_PASS");
 
+        setIfPresent(dotenv, "TWILIO_SID");
+        setIfPresent(dotenv, "TWILIO_TOKEN");
+        setIfPresent(dotenv, "TWILIO_NUM");
+
+    }
+
+    private void setIfPresent(Dotenv dotenv, String key) {
+        String value = dotenv.get(key);
+        if (value != null && !value.isBlank()) {
+            System.setProperty(key, value);
+        }
     }
 }
